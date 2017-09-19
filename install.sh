f751650dd1ad578313610cb98502f328a8362205
923115zx <923115zx@163.com>
2017-09-19 18:39:17 +0800

    Keep going.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 #      Author                      : Zhao Xin
 #      CreateTime                  : 2017-09-18 01:57:24 PM
 #      VIM                         : ts=4, sw=4
-#      LastModified                : 2017-09-18 22:53:57
+#      LastModified                : 2017-09-19 17:09:55
 #
 ########################################################################
 
@@ -16,6 +16,7 @@ source ./script/lib/common.sh
 SUPPORT_OS=("darwin" "centos" "redhat")
 SYSTEM=`uname -s`
 SUPPORTED=1
+CURRENT_PATH=$(cd `dirname $0`; pwd)
 
 case $SYSTEM in
 	Darwin)
@@ -40,21 +41,83 @@ if [ $SUPPORTED -eq 0 ]; then
 	exit 0
 fi
 
-if [ $SYSTEM != "darwin" ]; then
-	# yum setting.
-	get_rhel_centos_version
-	set_yum $? `pwd`
+if [ $CURRENT_OS = "darwin" ]; then
+	question="Install homebrew"
 else
-	# install homebrew. XXX: during then installation user need to type their passwd.
-	retries=3
-	while [ retries -ne 0 ]
-	do
-		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-		# Not verified, I am not sure $? will be 0 when above success.
-		if [ $? -eq 0 ]; then
-			break
-		fi
-		retries=$(($retries - 1))
-		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
-	done
+	question="Set yum"
 fi
+
+for ((i=0; i<3; ++i))
+do
+	pplain "$question? [y/n] "
+	read line
+	if [ $line != "y" ] && [ $line != "n" ]; then
+		pmsg "please input 'y' or 'n'"
+	else
+		break
+	fi
+done
+
+if [ $line = "y" ]; then
+	if [ $CURRENT_OS != "darwin" ]; then
+		# yum setting.
+#		get_rhel_centos_version
+		set_yum $CURRENT_PATH
+	else
+		# install homebrew. XXX: during then installation user need to type their passwd.
+		install_homebrew
+	fi
+fi
+
+install_script_and_config $CURRENT_PATH
+#last_cmd_ok "$question failed! quit."
+install_ycm $CURRENT_PATH
+
+#function pmenu ()
+#{
+#	while
+#	do
+#		pplain "========================================="
+#		pplain "1. Prepare package manager(rhel/centos is yum, darwin is homebrew)."
+#		pplain "2. Install script and set personal profile."
+#		pplain "3. Install YouComplete plugin for vim."
+#		pplain "4. Do 1, 2, 3 in order."
+#		pplain "5. exit."
+#		echo ""
+#		pplain "enter you choose:"
+#		read line
+#		case $line in
+#			1)
+#				if [ $CURRENT_OS != "darwin" ]; then
+#					set_yum $CURRENT_PATH
+#				else
+#					install_homebrew
+#				fi ;;
+#			2)
+#				install_script_and_config $CURRENT_PATH ;;
+#			3)
+#				if [ $CURRENT_OS != "darwin" ]; then
+#					if_yum_setted
+#					if [ $? -eq -1 ]; then
+#						pplain "Yum is not setted, please set yum first"
+#					else
+#						install_ycm
+#					fi
+#				else
+#					if_brew_installed
+#					if [ $? -eq -1 ]; then
+#						pplain "Homebrew is not installed, please install homebrew first"
+#					else
+#						install_ycm
+#					fi
+#				fi ;;
+#			4)
+#				;;
+#			5)
+#				pinfo "Goodbye."
+#				exit 0 ;;
+#			*)
+#				pinfo "Unknow option." ;;
+#		esac
+#	done
+#}
--- script/lib/common.sh
+++ script/lib/common.sh
@@ -4,20 +4,23 @@
 #      Author                      : Zhao Xin
 #      CreateTime                  : 2017-09-18 03:43:31 PM
 #      VIM                         : ts=4, sw=4
-#      LastModified                : 2017-09-18 22:51:53
+#      LastModified                : 2017-09-19 18:38:35
 #
 ########################################################################
 
 #!/bin/sh
 
 YumPath=/etc/yum.repos.d
+YumRecFile=/var/log/.yum_setted
+ScriptRecFile=/var/log/.script_setted
 
 function last_cmd_ok ()
 {
+	res=$?
 	# '$?' is the result of last cmd.
-	if [ $? -ne 0 ]; then
-		echo -e "${Red}Last cmd failed! Quit${Blank}"
-		exit $?
+	if [ ${res} -ne 0 ]; then
+		perror $1
+		exit ${res}
 	fi
 }
 
@@ -33,23 +36,160 @@ function get_rhel_centos_version ()
 		fi
 		if [ "$main_ver" != "6" ] && [ "$main_ver" != "7" ]; then
 			perror "Only 6.x and 7.x be supported. Sorry."
-			exit -1
+			return -1
 		fi
 		pmsg "found, main_ver=${main_ver}"
 		return $main_ver
 	done
 	perror "Not found main version in release msg. quit."
-	exit -1
+	return -1
+}
+
+function if_yum_setted ()
+{
+	if [ -e $YumRecFile ]
+		return 0
+	fi
+	return -1
 }
 
 function set_yum ()
 {
+	if_yum_setted
+	if [ $? -eq 0 ]; then
+		pinfo "Yum has be setted."
+		return $?
+	fi
+
+	get_rhel_centos_version
+	if [ $? -eq -1 ]; then
+		return $?
+	fi
+
+	ver=$?
 	pinfo "Set Yum Repo"
 	mkdir -p $YumPath
-	cp $2/config/Centos-${1}.repo yum.repo.d/
+	cp $1/config/Centos-${ver}.repo yum.repo.d/
 	cd $YumPath
-	yum -y install epel-release && rpm -Uvh https://li.nux.ro/download/nux/dextop/el${1}/x86_64/nux-dextop-release-0-1.el${1}.nux.noarch.rpm
+	yum -y install epel-release && rpm -Uvh https://li.nux.ro/download/nux/dextop/el${ver}/x86_64/nux-dextop-release-0-1.el${ver}.nux.noarch.rpm
 	yum clean all && yum makecache && yum -y update
 	cd -
+
+	touch $YumRecFile
+
 	pinfo "Set Yum Repo Done"
 }
+
+function if_brew_installed ()
+{
+	findbrew=`which brew`
+	echo $findbrew | grep -q "no brew"
+	if [ $? -ne 0 ]; then
+		return 0
+	fi
+	return -1
+}
+
+function install_homebrew ()
+{
+	if_brew_installed
+	if [ $? -eq 0 ]; then
+		pinfo "Homebrew has already installed"
+		return $?
+	fi
+
+	retries=3
+	while [ $retries -ne 0 ]
+	do
+		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+		# Not verified, I am not sure $? will be 0 when above success.
+		if [ $? -eq 0 ]; then
+			break
+		fi
+		retries=$(($retries - 1))
+		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
+	done
+
+	if [ $retries -eq 0 ]; then
+		perror "Fail to install homebrew."
+		return -1
+	fi
+
+	pinfo "Install homebrew success."
+	return 0
+}
+
+function install_script_and_config ()
+{
+	if [ -e $ScriptRecFile ]; then
+		pinfo "script has been setted"
+		return 0
+	fi
+
+	pinfo "Script and config installing"
+	mkdir -p $HOME/bin
+	cd $1/script/bin
+	cp $(`ls`) $HOME/bin
+	cd -
+
+	echo "alias mktest=\". mktest.sh\"" >> $HOME/.bash_profile
+	echo "alias shtest=\". shtest.sh\"" >> $HOME/.bash_profile
+	echo "alias asmtest=\". asmtest.sh\"" >> $HOME/.bash_profile
+	echo "alias git_info=\". git_info.sh\"" >> $HOME/.bash_profile
+	echo "alias svn_diff=\". svn_diff.sh\"" >> $HOME/.bash_profile
+
+	# Not necessary.
+#	mkdir -p $HOME/.gdbinit
+#	echo "set disassembly-flavor intel" > $HOME/.gdbinit
+
+	touch $ScriptRecFile
+	pinfo "scripts and config installed"
+	return 0
+}
+
+function install_ycm ()
+{
+	if [ -e $HOME/.vim/plugin/youcompleteme.vim ]; then
+		pinfo "YouCompleteMe already be installed"
+		return 0
+	fi
+
+	if [ $CURRENT_OS = "darwin" ]; then
+		if_brew_installed
+		if [ $? -eq -1 ]; then
+			perror "Homebrew not installed, please check the installation or reinstall it"
+			exit -1
+		fi
+		packageManager=brew
+	else
+		if_yum_setted
+		if [ $? -eq -1 ]; then
+			perror "Yum not be set, please check configuration or reset it"
+			exit -1
+		fi
+		packageManager="yum -y"
+	fi
+
+	$packageManager python
+	$packageManager python-devel
+
+	$packageManager cscope
+
+	if [ "`gcc --version`" == "" ]; then
+		pplain "Gcc is not installed, install gcc and g++"
+		$packageManager install gcc
+		$packageManager install gcc-c++
+		$packageManager install libstdc++-devel
+		pplain "Gcc and G++ installation finished"
+	fi
+
+	$packageManager install cmake
+
+	git clone https://github.com/Valloric/YouCompleteMe.git
+	cd ./YouCompleteMe
+	git submodule update --init --recursive
+
+	# TODO
+
+}
+
