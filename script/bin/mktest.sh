04550be182f9a28b0cb74851dfb7ed0a8f114677
923115zx <923115zx@163.com>
2017-10-28 23:02:29 +0800

    Change mktest.sh.

--- script/bin/mktest.sh
+++ script/bin/mktest.sh
@@ -15,39 +15,51 @@ touch $buildFile
 echo "#!/bin/sh" >> $buildFile
 echo "g++ -g -std=c++11 ${1}.cc -o $1" >> $buildFile
 chmod 744 $buildFile
+ccfile=${1}.cc
 
-echo "#include <stdio.h>" >> ${1}.cc
-echo "#include <stdlib.h>" >> ${1}.cc
-echo "#include <string.h>" >> ${1}.cc
-echo "#include <map>" >> ${1}.cc
-echo "#include <vector>" >> ${1}.cc
-echo "#include <set>" >> ${1}.cc
-echo "#include <string>" >> ${1}.cc
-echo "" >> ${1}.cc
-echo "using std::vector;" >> ${1}.cc
-echo "using std::map;" >> ${1}.cc
-echo "using std::set;" >> ${1}.cc
-echo "using std::string;" >> ${1}.cc
-echo "" >> ${1}.cc
-echo "//struct TreeNode" >> ${1}.cc
-echo "//{" >> ${1}.cc
-echo -e "//\tint val;" >> ${1}.cc
-echo -e "//\tTreeNode* left;" >> ${1}.cc
-echo -e "//\tTreeNode* right;" >> ${1}.cc
-echo -e "//\tTreeNode(int x) : val(x), left(NULL), right(NULL) {}" >> ${1}.cc
-echo -e "//}\n" >> ${1}.cc
-echo "//struct ListNode" >> ${1}.cc
-echo "//{" >> ${1}.cc
-echo -e "//\tint val;" >> ${1}.cc
-echo -e "//\tListNode* next;" >> ${1}.cc
-echo -e "//\tListNode(int x) : val(x), next(NULL) {}" >> ${1}.cc
-echo -e "//}\n" >> ${1}.cc
-echo "int main(int argc, char* argv[])" >> ${1}.cc
-echo "{" >> ${1}.cc
-echo "	(void)argc;" >> ${1}.cc
-echo "	(void)argv;" >> ${1}.cc
-echo "" >> ${1}.cc
-echo "	return 0;" >> ${1}.cc
-echo "}" >> ${1}.cc
+function add_line
+{
+	echo -e $1 >> $ccfile
+}
+
+add_line "#include <stdio.h>"
+add_line "#include <stdlib.h>"
+add_line "#include <string.h>"
+add_line "#include <map>"
+add_line "#include <vector>"
+add_line "#include <set>"
+add_line "#include <string>"
+add_line "#include <queue>"
+add_line ""
+add_line "using std::vector;"
+add_line "using std::map;"
+add_line "using std::set;"
+add_line "using std::string;"
+add_line "using std::queue;"
+add_line ""
+add_line "//struct TreeNode"
+add_line "//{"
+add_line "//\tint val;"
+add_line "//\tTreeNode* left;"
+add_line "//\tTreeNode* right;"
+add_line "//\tTreeNode(int x) : val(x), left(NULL), right(NULL) {}"
+add_line "//};\n"
+add_line "//struct ListNode"
+add_line "//{"
+add_line "//\tint val;"
+add_line "//\tListNode* next;"
+add_line "//\tListNode(int x) : val(x), next(NULL) {}"
+add_line "//};\n"
+add_line "class solution"
+add_line "{"
+add_line "public:"
+add_line "};\n"
+add_line "int main(int argc, char* argv[])"
+add_line "{"
+add_line "	(void)argc;"
+add_line "	(void)argv;"
+add_line ""
+add_line "	return 0;"
+add_line "}"
 
 prepareProject.sh
